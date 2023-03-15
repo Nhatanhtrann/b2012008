2dee57ca48c05315711b6813c06692634e712b5f
Nhatanhtrann <91231766+Nhatanhtrann@users.noreply.github.com>
2023-03-15 19:18:06 +0700

    Add files via upload

---
+++ dangnhap.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Thức ăn chăn nuôi gia súc</title>
+            <link href="./css/style.css" rel="stylesheet">
+            <link href="./css/megamenu.css" rel="stylesheet">
+            <link href="./css/theme.css" rel="stylesheet">
+            <link rel="stylesheet" href="./css/form.css">
+            <script src="js/form.js"></script>
+            <link rel="stylesheet" href=".https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            <link rel="stylesheet" href="./fontawesome-free-6.3.0-web/css/all.min.css">
+            <script type="text/javascript" src="./js/jquery.js"></script>
+            <script type="text/javascript" src="./js/jquery-migrate.js"></script>
+            <script type="text/javascript" src="./js/plugins.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-3887203-14"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'UA-3887203-14');
+        </script>
+        <script>
+var current = null;
+document.querySelector('#email').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#password').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#submit').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -730,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '530 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+        </script>
+        <!-- Google Tag Manager -->
+        <script>
+            (function(w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({
+                    'gtm.start': new Date().getTime(),
+                    event: 'gtm.js'
+                });
+                var f = d.getElementsByTagName(s)[0]
+                  , j = d.createElement(s)
+                  , dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            }
+            )(window, document, 'script', 'dataLayer', 'GTM-TQFLQNS');
+        </script>
+        <!-- End Google Tag Manager -->
+    </head>
+    <body class="dark-header org-dark-header sticky_header">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.php?id=GTM-TQFLQNS" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        <div id="fb-root"></div>
+        <script type="application/ld+json">
+             { "@@context": "https://schema.org", "@@type": "Company", "name": "CP Sản xuất & Thương mại Hoàn Dương - ALASKA Feedmill", "url": "https://hoanduong.com", "logo": "https://hoanduong.com/img/logo0.png", "contactPoint": { "@@type": "ContactPoint", "telephone": "02263582027", "contactType": "customer service", "areaServed": "VN", "availableLanguage": "Vietnamese" }, "sameAs": [ "https://www.facebook.com/alaskafeedmill/", "https://www.youtube.com/channel/UCb_wgMxF4_L86fIrbf-9Ptg", "https://hoanduong.com/" ] } 
+        </script>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v5.0&appId=1387291875003558&autoLogAppEvents=1"></script>
+        <div id="wrapper">
+            <div class="header-wrapper before-sticky">
+                <div id="top-bar">
+                    <div class="row">
+                        <div class="large-12 columns">
+                            <div class="right-text right">
+                                <ul class="top-bar-nav">
+                                    <li>
+                                        <a href="./index.php">Trang chủ</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./gioithieu.php">Giới thiệu</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./tintuc.php">Tin tức</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./doitac.php">Đối tác</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./lienhe.php">Liên hệ</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./tuyendung.php">Tuyển dụng</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Đã sử lý đến đây -->
+                <header id="masthead" class="site-header">
+                    <div class="row">
+                        <div class="large-12 columns header-container">
+                            <div id="logo">
+                                <a href="./" title="Cái tên nhóm" rel="home">
+                                    <img src="./img/logo0.png" alt="Tên nhóm"/>
+                                </a>
+                            </div>
+                            <div class="right-links">
+                                <div class="mobile-menu show-for-small">
+                                    <a href=".#jPanelMenu" class="off-canvas-overlay" data-pos="left" data-color="light">
+                                        <span class="fa fa-bars"></span>
+                                    </a>
+                                </div>
+                                <div class="wide-nav-search hide-for-small right">
+                                    <div class="row collapse search-wrapper">
+                                        <form role="search" method="get" action="/tim-kiem/">
+                                            <div class="lang-nav">
+                                                <a href="./?lang=en">English</a>
+                                                <a class="active" href="./?lang=vn">Việt Nam</a>
+                                            </div>
+                                            <div class="large-10 small-10 columns">
+                                                <input type="search" value="" name="s" placeholder="Tìm kiếm"/>
+                                            </div>
+                                            <div class="large-2 small-2 columns">
+                                                <button type="submit" class="button secondary postfix">
+                                                    <i class="fa fa-search"></i>
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <nav>
+                    <content>
+                        <div class="content__page">
+                            <div class="content__container">
+                                
+                                <div class="content__container-right">
+                                    <div class="login">
+                                        <form action="/action_page.php" method="post" class="login" id="form-1">
+                                            <h1 class="login-heading">Đăng nhập</h1>
+                                            <div class="form-group">
+        
+                                            <div class="form-group">
+                                                <label for="email" class="form-label">Email:</label>
+                                                <input id="email" name="email" type="text" placeholder="VD: XXXX@gmail.com" class="form-control">
+                                                <span class="form-message"></span>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="password" class="form-label">Mật khẩu:</label>
+                                                <input id="password" name="password" type="password" placeholder="******" class="form-control">
+                                                <span class="form-message"></span>
+                                            </div>
+                                                <button onclick="myFunction()" class="form-submit" style="margin: 30px auto;">Đăng nhập</button>
+                                                <label class="form-label" style="padding-left: 38.5%;">Bạn chưa có tài khoản?<a href="./dangki.php">Đăng kí.</a></label>
+                                                <label class="form-label" style="padding-left: 38.5%;">Hoặc nếu bạn là admin thì đăng nhập ở đây!<a href="../">Đăng nhập với vai trò admin.</a></label>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+    
+                    </content>
+                </nav>
+                
+                <footer class="footer-wrapper" role="contentinfo">
+                    <div class="footer dark" style="background:#29ade5;">
+                        <div class="row">
+                            <div class="large-4 columns widget left widget_nav_menu hide-for-small">
+                                <h3 class="widget-title">Sản phẩm - Dịch vụ</h3>
+                                <div class="tx-div small"></div>
+                                <ul class="menu">
+                                    <li class="menu-item">
+                                        <a href="./thuc-an-chan-nuoi.php">Thức ăn chăn nuôi</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./dich-vu-tu-van.php">Dịch vụ tư vấn</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./nguyen-lieu.php">Nguyên liệu</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="large-4 columns widget left widget_nav_menu hide-for-small">
+                                <h3 class="widget-title">Nội dung chính</h3>
+                                <div class="tx-div small"></div>
+                                <ul class="menu">
+                                    <li>
+                                        <a href="./index.php">Trang chủ</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./gioithieu.php">Giới thiệu</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./tintuc.php">Tin tức</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./doitac.php">Đối tác</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./lienhe.php">Liên hệ</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./tuyendung.php">Tuyển dụng</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="large-4 columns widget left widget_text">
+                                <h3 class="widget-title">GIA SÚC FOOD</h3>
+                                <div class="tx-div small"></div>
+                                <div class="textwidget">
+                                    <p>
+                                        <span style="color:#fff;">Địa chỉ: Khu II, Đ. 3/2, Xuân Khánh, Ninh Kiều, Cần Thơ, Việt Nam</span>
+                                        <br/>
+                                        <span class="small-font">
+                                            Điện thoại: 079 544 6708<br/>Fax: 079 544 6708
+                                        </span>
+                                    </p>
+                                    <!-- <p>
+                                        <span style="color:#fff;">Nhà máy thức ăn chăn nuôi Alaska, Khu công nghiệp Đồng Văn, H. Duy Tiên, Hà Nam</span>
+                                        <br/>
+                                        <span class="small-font">
+                                            Điện thoại: (022) 63 582 027 - 63 691 966<br/>
+                                            Fax: (022) 63 836 977<span>
+                                    </p> -->
+                                </div>
+                                <div class="fb-page" data-href=".https://www.facebook.com/CTUDHCT" data-tabs="" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                                    <blockquote cite="https://www.facebook.com/CTUDHCT" class="fb-xfbml-parse-ignore">
+                                        <a href=".https://www.facebook.com/CTUDHCT"></a>
+                                    </blockquote>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="absolute-footer dark" style="background-color:#003351;">
+                        <div class="row">
+                            <div class="large-12 columns">
+                                <div class="left">
+                                    <div class="copyright-footer">
+                                        Copyright &copy;2023 - <strong></strong>
+                                        . All rights reserved.
+                                    </div>
+                                </div>
+                                <div class="right">
+                                    Social: <a href=".https://www.facebook.com/CTUDHCT"><i class="fa-brands fa-facebook"></i></a>
+                                    / <a href=".https://zalo.me/g/vvzjgb690">Zalo page</a>
+                                    / <a href=".http://facebook.com/vquoc02"><i class="fa-brands fa-facebook-messenger"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                <a id="top-link" class="animated fadeInUp" onclick="window.scrollTo({ top: 0, behavior: 'smooth' })">
+                    <span class="fa fa-angle-up"></span>
+                </a>
+                <script type="text/javascript" src="/js/flatsome.js"></script>
+                <script type="text/javascript" src="/js/frontend.js"></script>
+                <script type="text/javascript" src="/js/form.js"></script>
+        </body>
+</html>
